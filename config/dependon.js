1315c056029db7e0a63e6bd94a94b15a2bc406df
王。 <atmwjf@gmail.com>
2016-05-31 14:05:58 +0800

    通过资源依赖表来管理依赖关系

--- config/dependon.js
+++ config/dependon.js
@@ -1,7 +1,14 @@
 module.exports = {
-    'font':{
-        'plugina':['plugina'],
-        'dialog':['dialog']
-    }
-    
-};
+	combine : {
+		'mo':['mo-core','mo-xpost','mo-dialog' ,'jquery'],
+		'abc':['c','b','c', 'diloag'],
+		'xyz':['dialog-login', 'com:header'],
+		'font':['lib:x']
+
+	},
+	page : {
+		font: ['mo', 'font'],
+		login:['mo', 'abc']
+
+	}
+}
